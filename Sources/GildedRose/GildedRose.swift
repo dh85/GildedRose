3ff1f3429d3b217dcf611a55fc5946d7a931a148
David Hughes <39618866+dh85@users.noreply.github.com>
2023-08-01 14:19:14 +0200

    Initial Commit

---
+++ Sources/GildedRose/GildedRose.swift
@@ -0,0 +1,6 @@
+public struct GildedRose {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/GildedRoseTests/GildedRoseTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import GildedRose
+
+final class GildedRoseTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(GildedRose().text, "Hello, World!")
+    }
+}
