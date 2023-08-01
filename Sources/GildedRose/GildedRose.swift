f358285435b3dce890fd2ce9776147fd4f5a1045
David Hughes <39618866+dh85@users.noreply.github.com>
2023-08-01 14:39:05 +0200

    Move update item to separate function as a static function outside the GildedRose class (i.e. nothing in the update function is depending on GildedRose)

--- Sources/GildedRose/GildedRose.swift
+++ Sources/GildedRose/GildedRose.swift
@@ -7,52 +7,56 @@ class GildedRose {
 
     func updateQuality() {
         for item in items {
-            if item.name != "Aged Brie", item.name != "Backstage passes to a TAFKAL80ETC concert" {
+            update(item: item)
+        }
+    }
+}
+
+func update(item: Item) {
+    if item.name != "Aged Brie", item.name != "Backstage passes to a TAFKAL80ETC concert" {
+        if item.quality > 0 {
+            if item.name != "Sulfuras, Hand of Ragnaros" {
+                item.quality = item.quality - 1
+            }
+        }
+    } else {
+        if item.quality < 50 {
+            item.quality = item.quality + 1
+
+            if item.name == "Backstage passes to a TAFKAL80ETC concert" {
+                if item.sellIn < 11 {
+                    if item.quality < 50 {
+                        item.quality = item.quality + 1
+                    }
+                }
+
+                if item.sellIn < 6 {
+                    if item.quality < 50 {
+                        item.quality = item.quality + 1
+                    }
+                }
+            }
+        }
+    }
+
+    if item.name != "Sulfuras, Hand of Ragnaros" {
+        item.sellIn = item.sellIn - 1
+    }
+
+    if item.sellIn < 0 {
+        if item.name != "Aged Brie" {
+            if item.name != "Backstage passes to a TAFKAL80ETC concert" {
                 if item.quality > 0 {
                     if item.name != "Sulfuras, Hand of Ragnaros" {
                         item.quality = item.quality - 1
                     }
                 }
             } else {
-                if item.quality < 50 {
-                    item.quality = item.quality + 1
-
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert" {
-                        if item.sellIn < 11 {
-                            if item.quality < 50 {
-                                item.quality = item.quality + 1
-                            }
-                        }
-
-                        if item.sellIn < 6 {
-                            if item.quality < 50 {
-                                item.quality = item.quality + 1
-                            }
-                        }
-                    }
-                }
+                item.quality = item.quality - item.quality
             }
-
-            if item.name != "Sulfuras, Hand of Ragnaros" {
-                item.sellIn = item.sellIn - 1
-            }
-
-            if item.sellIn < 0 {
-                if item.name != "Aged Brie" {
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert" {
-                        if item.quality > 0 {
-                            if item.name != "Sulfuras, Hand of Ragnaros" {
-                                item.quality = item.quality - 1
-                            }
-                        }
-                    } else {
-                        item.quality = item.quality - item.quality
-                    }
-                } else {
-                    if item.quality < 50 {
-                        item.quality = item.quality + 1
-                    }
-                }
+        } else {
+            if item.quality < 50 {
+                item.quality = item.quality + 1
             }
         }
     }
